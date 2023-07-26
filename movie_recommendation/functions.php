9feaee6c127329c0953a0e351300ac4f1908e5f8
afzal2208 <mohammadafzal2208@gmail.com>
2023-07-26 17:53:48 +0100

    masters project

---
+++ movie_recommendation/functions.php
@@ -0,0 +1,75 @@
+<?php
+require_once( "sparql_library.php" );
+
+class Functions{
+    private $db;
+    
+    public function __construct(){
+	$db = sparql_connect( "http://localhost:3030/movie/query" );
+	if( !$db ) { print sparql_errno() . ": ---" . sparql_error(). "\n"; exit; }
+	sparql_ns( "moviedata","http://www.movierecommendation_ontology/#" );
+    }
+
+    public function get_moviesdata(){
+        $sparql = "SELECT ?title ?desc ?duration ?date ?url ?ratings
+        WHERE {
+          ?var a moviedata:Movie.
+          ?var moviedata:title ?title.
+          ?var moviedata:description ?desc.
+          ?var moviedata:duration ?duration.
+          ?var moviedata:release_date ?date.
+          ?var moviedata:url ?url.
+          ?var moviedata:ratings ?ratings
+          
+        }";
+        $result = sparql_query( $sparql ); 
+        return $result;
+    }
+
+    public function get_reviews(){
+        $sparql = "SELECT ?title ?url ?review
+        WHERE {
+          ?var a moviedata:Movie.
+          ?var moviedata:title ?title.
+          ?var moviedata:url ?url.
+          ?var moviedata:has_review ?review
+        }";
+        $result = sparql_query( $sparql ); 
+        return $result;
+    }
+
+    public function get_classics(){
+      $sparql = "SELECT DISTINCT ?title ?desc ?url ?date
+      WHERE {
+        ?var a moviedata:Movie.
+        ?var moviedata:ratings 'classic'.
+        ?var moviedata:title ?title.
+        ?var moviedata:description ?desc.
+        ?var moviedata:url ?url.
+        ?var moviedata:release_date ?date
+      }
+      ORDERBY (?title)";
+      $result = sparql_query( $sparql ); 
+      return $result;
+  }
+
+  public function get_newreleases(){
+    $sparql = "SELECT DISTINCT ?title ?desc ?url ?date
+    WHERE {
+      ?var a moviedata:Movie.
+      ?var moviedata:ratings 'new'.
+      ?var moviedata:title ?title.
+      ?var moviedata:description ?desc.
+      ?var moviedata:url ?url.
+      ?var moviedata:release_date ?date
+    }
+    ORDERBY (?title)";
+    $result = sparql_query( $sparql ); 
+    return $result;
+}
+
+   
+   
+
+}
+?>
---
+++ movie_recommendation/index.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<?php
+include( "functions.php" );
+
+$obj =  new Functions();
+ $result = $obj->get_moviesdata(); 
+?>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/movie_recommendation/images/favicon.ico">
+
+    <title>Online Movie Recommendations/ Reviews</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="/movie_recommendation/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="/movie_recommendation/css/album.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <header>
+      <div class="navbar navbar-dark bg-dark box-shadow">
+        <div class="container d-flex justify-content-between">
+          <a href="#" class="navbar-brand d-flex align-items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mr-2"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path><circle cx="12" cy="13" r="4"></circle></svg>
+            <strong>Movie Fun</strong>
+
+  <ul><a href="newreleases.php">New Releases</a></ul>
+  <ul><a href="recommendations.php">Recommendations</a></ul>
+  <ul><a href="classics.php">Classics</a></ul>
+  <ul><a href="reviews.php">Reviews</a></ul>
+  <ul><a href="genres.php">Genres</a></ul>
+          </a>
+          <p style="color:white;">User</p>
+        </div>
+      </div>
+    </header>
+
+    <main role="main">
+
+      <div class="album py-5 bg-light">
+        <div class="container">
+
+          <div class="row">
+          <?php
+		while( $row = sparql_fetch_array( $result ) )
+		{
+		?>            <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <img src='<?php echo($row["url"]); ?>' data-src="holder.js" alt="Card image cap" class="card-img-top img-fluid" style="width: 22rem; height: 20rem;"/>
+                  <div class="card-body">
+                  <p class="card-text"><b>Title: </b><?php echo $row["title"]; ?></p>
+                  <p class="card-text"><b>Description: </b><?php echo $row["desc"]; ?></p>
+                  <p class="card-text"><b>Ratings: </b><?php echo $row["ratings"]; ?></p>
+                  <p class="card-text"><b>Duration: </b><?php echo $row["duration"]; ?></p>
+                  <p class="card-text"><b>Release Date: </b><?php echo $row["date"]; ?></p>
+                </div>
+              </div>
+            </div>
+            <?php } ?>
+                
+            
+          </div>
+        </div>
+      </div>
+
+    </main>
+
+    <footer class="text-muted">
+      <div class="container">
+        <p class="float-right">
+        <!--  <a href="#">Back to top</a> -->
+        </p>
+        <p></p>
+      </div>
+    </footer>
+
+  <script src="/movie_recommendation/js/holder.min.js"></script>
+  </body>
+</html>
